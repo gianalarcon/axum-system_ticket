22935b6a8f88281bc45dbf628f2293ee7d4cf05e
gianmarcoalarcon <gian.alarcon@unmsm.edu.pe>
2023-05-16 18:59:15 +0800

    Feat: Add ctx in fn mw_require_auth()

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ use crate::model::ModelController;
 
 pub use self::error::{Error, Result};
 
+mod ctx;
 mod error;
 mod model;
 mod web;
--- src/web/mw_auth.rs
+++ src/web/mw_auth.rs
@@ -1,17 +1,56 @@
-use axum::{http::Request, middleware::Next, response::Response};
+use async_trait::async_trait;
+use axum::http::request::Parts;
+use axum::RequestPartsExt;
+use axum::{extract::FromRequestParts, http::Request, middleware::Next, response::Response};
+use lazy_regex::regex_captures;
 use tower_cookies::Cookies;
 
-use crate::{web::AUTH_TOKEN, Error, Result};
+use crate::{ctx::Ctx, web::AUTH_TOKEN, Error, Result};
 
 pub async fn mw_require_auth<B>(
-  cookies: Cookies,
+  ctx: Result<Ctx>,
   req: Request<B>,
   next: Next<B>,
 ) -> Result<Response> {
   println!("->> {:<12} -- mw_require_auth", "MIDDLEWARE");
 
-  let auth_token = cookies.get(AUTH_TOKEN).map(|c| c.value().to_string());
-  //Todo: REal auth-token parsing and validation
-  auth_token.ok_or(Error::AuthFailNoAuthTokenCookie)?;
+  ctx?;
+
   Ok(next.run(req).await)
+}
+
+// region     --- Ctx Extractor
+#[async_trait]
+impl<S: Send + Sync> FromRequestParts<S> for Ctx {
+  type Rejection = Error;
+
+  async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self> {
+    println!("->> {:<12} -- Ctx", "EXTRACTOR");
+    // User the cookies extractor.
+    let cookies = parts.extract::<Cookies>().await.unwrap();
+
+    let auth_token = cookies.get(AUTH_TOKEN).map(|c| c.value().to_string());
+
+    // Parse token
+    let (user_id, exp, sign) = auth_token
+      .ok_or(Error::AuthFailNoAuthTokenCookie)
+      .and_then(parse_token)?;
+
+    Ok(Ctx::new(user_id))
+  }
+}
+// endregion: --- Ctx Extractor
+
+fn parse_token(token: String) -> Result<(u64, String, String)> {
+  let (_whole, user_id, exp, sign) = regex_captures!(
+    r#"^user-(\d+)\.(.+)\.(.+)"#, // a literal regex
+    &token
+  )
+  .ok_or(Error::AuthFailTokenWrongFormat)?;
+
+  let user_id: u64 = user_id
+    .parse()
+    .map_err(|_| Error::AuthFailTokenWrongFormat)?;
+
+  Ok((user_id, exp.to_string(), sign.to_string()))
 }
